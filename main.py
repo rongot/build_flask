ef872e947a8cf3f4927c119112617a84662460fd
rongot <rgotliv@gmail.com>
2023-08-26 16:23:28 +0000

    update
    update1

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 # from replit.web import App
 
 app = Flask(__name__)
 
 # print(__name__)
+JOBS = [{
+  "id": 1,
+  "title": "Qa",
+  "location": "new delhk",
+  "salary": 12000
+}, {
+  "id": 2,
+  "title": "BD",
+  "location": "new pop",
+  "salary": 13000
+}, {
+  "id": 3,
+  "title": "DB",
+  "location": "sal",
+  "salary": 142000
+}, {
+  "id": 4,
+  "title": "CTO",
+  "location": "rabak",
+}]
 
 
 @app.route('/')
 def index():
-  return "hi world"
+  return render_template('home.html', jobs=JOBS, rootname="ronen")
+
+
+@app.route('/getRoute/')
+def getRoute():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
